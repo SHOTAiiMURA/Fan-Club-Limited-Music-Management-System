b030f547b6b44c1256509b1a9f7354d86cc18d1b
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-06-16 23:56:16 +0900

    created music interface

---
+++ untitled/src/SingleImp.java
@@ -0,0 +1,30 @@
+public class SingleImp implements SingleInterface {
+    //create fields
+    int single_id;
+
+
+    @Override
+    public void addSingle() {
+
+    }
+
+    @Override
+    public void editSingle() {
+
+    }
+
+    @Override
+    public void deleteSingle() {
+
+    }
+
+    @Override
+    public void saveSingle() {
+
+    }
+
+    @Override
+    public void searchSingle() {
+
+    }
+}
